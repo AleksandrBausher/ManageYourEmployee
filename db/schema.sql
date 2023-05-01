a5bcf0cce353d9117fbe7c71db3b0a96362e40ca
AleksandrBausher <bausheraleksandr@gmail.com>
2023-05-01 12:54:25 -0400

    created schema for sql database

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employeeTrackerDB;
+CREATE DATABASE employeeTrackerDB;
+
+USE employeeTrackerDB;
+
+
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT FOREIGN KEY REFERENCES ON DELETE SET NULL,
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT FOREIGN KEY REFERENCES role(id) ON DELETE SET NULL,
+    manager_id INT FOREIGN KEY REFERENCES employee(id) ON DELETE SET NULL,
+);
