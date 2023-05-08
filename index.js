be4ad1ffa72f99abd2090096c2ee7ee2835252dd
AleksandrBausher <bausheraleksandr@gmail.com>
2023-05-08 11:17:09 -0400

    completed view employee, role and department with proper fields like manager title salary and department

--- index.js
+++ index.js
@@ -10,13 +10,6 @@ const menuOptions = [
   "Add role",
   "View all departments",
   "Add department",
-  "Update employee managers",
-  "View employees by manager",
-  "View employees by department",
-  "Delete departments",
-  "Delete role",
-  "Delete employee",
-  "View the total utilized budget of a department",
   "Exit",
 ];
 
@@ -34,31 +27,6 @@ const addDepartmentPrompts = [
     type: "input",
     name: "department_name",
     message: "What is the department name",
-  },
-];
-
-const deleteEmployeePrompts = [
-  {
-    type: "list",
-    name: "chosenOption",
-    message: "Which employee would you like to delete?",
-    choices: [],
-  },
-];
-const deleteRolePrompts = [
-  {
-    type: "list",
-    name: "chosenOption",
-    message: "Which role would you like to delete?",
-    choices: [],
-  },
-];
-const deleteDepartmentPrompts = [
-  {
-    type: "list",
-    name: "chosenOption",
-    message: "Which department would you like to delete?",
-    choices: [],
   },
 ];
 
@@ -80,7 +48,49 @@ const viewDepartmentPrompts = [
 ];
 
 async function viewAllEmployees(db) {
-  const [rows, field] = await db.execute("SELECT * from employee");
+  const [employees, e] = await db.execute("SELECT * from employee");
+  const [managers, m] = await db.execute("SELECT * from employee");
+  const [roles, r] = await db.execute("SELECT * from role");
+  const [departments, d] = await db.execute("SELECT * from department");
+  employees.forEach((employee)=>{
+    roles.forEach((role)=>{
+      if(employee.role_id == role.id){
+        employee.title = role.title;
+        employee.salary = role.salary
+        departments.forEach((department)=>{
+          if(role.department_id ==department.id){
+            employee.department = department.name
+          }
+        })
+        delete employee.role_id
+      }
+    })
+    managers.forEach((manager)=>{
+      if(employee.manager_id == manager.id){
+        employee.manager = manager.first_name +" "+manager.last_name
+      }
+    })
+    delete employee.manager_id
+  })
+  console.table(employees);
+}
+
+async function viewAllRoles(db) {
+  const [rows, r] = await db.execute("SELECT * from role");
+  const [departs, d] = await db.execute(`SELECT * from department`);
+  rows.forEach(async function (element) {
+    departs.forEach((departElement) => {
+      if (element.department_id == departElement.id) {
+        element.department = departElement.name;
+        delete element.department_id;
+      }
+    });
+  });
+  console.table(rows);
+}
+
+async function viewAllDepartments(db) {
+  const [rows, field] = await db.execute("SELECT * from department");
   console.table(rows);
 }
 
@@ -123,7 +133,11 @@ async function addEmployee(db) {
   await db.execute(
     `INSERT INTO employee ( first_name, last_name, role_id, manager_id) VALUES ("${newEmployee.first_name}","${newEmployee.last_name}",${newEmployee.role_id},${newEmployee.manager_id});`
   );
-  console.log(`Employee ${newEmployee.first_name+" "+newEmployee.last_name} added to Database`);
+  console.log(
+    `Employee ${
+      newEmployee.first_name + " " + newEmployee.last_name
+    } added to Database`
+  );
 }
 async function addRole(db) {
   const [departments, d] = await db.execute("SELECT * FROM department");
@@ -145,31 +159,52 @@ async function addRole(db) {
     `INSERT INTO role (title, salary, department_id) VALUES ("${newRole.name}",${newRole.salary},${newRole.department_id});`
   );
   console.log(`Role "${newRole.name}" added to Database`);
-
 }
 async function addDepartment(db) {
   const newDepartment = await inquirer.prompt(addDepartmentPrompts);
   await db.execute(
     `INSERT INTO department (name) VALUES ('${newDepartment.department_name}');`
   );
-  console.log(`Department "${newDepartment.department_name}" added to Database`);
+  console.log(
+    `Department "${newDepartment.department_name}" added to Database`
+  );
 }
 
-async function viewAllRoles(db) {
-  const [rows, field] = await db.execute("SELECT * from role");
-  rows.forEach(async function (element) {
-    const [departs, field] = await db.execute(
-      `SELECT * from department where id = '${element.department_id}'`
-    );
-    element.department = departs[0].name;
-    delete element.department_id;
+async function updateEmployeeRole(db) {
+  const [employees, e] = await db.execute("SELECT * FROM employee");
+  const [roles, r] = await db.execute("SELECT * FROM role");
+
+  employees.forEach((element) => {
+    element.value = element.id;
+    element.name = element.first_name + " " + element.last_name;
   });
 
-  console.table(rows);
-}
-async function viewAllDepartments(db) {
-  const [rows, field] = await db.execute("SELECT * from department");
-  console.table(rows);
+  roles.forEach((element) => {
+    element.value = element.id;
+    element.name = element.title;
+  });
+
+  const updateEmployeeRolePrompts = [
+    {
+      type: "list",
+      name: "id",
+      message: "Which employee to update?",
+      choices: employees,
+    },
+    {
+      type: "list",
+      name: "role",
+      message: "Which role would you like to give to the employee?",
+      choices: roles,
+    },
+  ];
+  const updatedRole = await inquirer.prompt(updateEmployeeRolePrompts);
+
+  await db.execute(
+    `UPDATE employee SET role_id=${updatedRole.role} WHERE id=${updatedRole.id}`
+  );
+
+  console.log(`Employee role have been updated`);
 }
 
 async function init() {
@@ -180,54 +215,34 @@ async function init() {
     database: "employeeTrackerDB",
   });
 
-  const chosenOption = await inquirer.prompt(menuPrompts);
-  switch (chosenOption.choice) {
-    case menuOptions[0]:
-      await viewAllEmployees(db);
-      break;
-    case menuOptions[1]:
-      await addEmployee(db);
-      break;
-    case menuOptions[2]:
-      console.log(menuOptions[2]);
-      break;
-    case menuOptions[3]:
-      await viewAllRoles(db);
-      break;
-    case menuOptions[4]:
-      await addRole(db);
-      break;
-    case menuOptions[5]:
-      await viewAllDepartments(db);
-      break;
-    case menuOptions[6]:
-      await addDepartment(db);
-      break;
-    case menuOptions[7]:
-      console.log(menuOptions[7]);
-      break;
-    case menuOptions[8]:
-      console.log(menuOptions[8]);
-      break;
-    case menuOptions[9]:
-      console.log(menuOptions[9]);
-      break;
-    case menuOptions[10]:
-      console.log(menuOptions[10]);
-      break;
-    case menuOptions[11]:
-      console.log(menuOptions[11]);
-      break;
-    case menuOptions[12]:
-      console.log(menuOptions[12]);
-      break;
-    case menuOptions[13]:
-      console.log(menuOptions[13]);
-      break;
-    case menuOptions[14]:
-      process.exit();
-      break;
+  while (true) {
+    const chosenOption = await inquirer.prompt(menuPrompts);
+    switch (chosenOption.choice) {
+      case menuOptions[0]:
+        await viewAllEmployees(db);
+        break;
+      case menuOptions[1]:
+        await addEmployee(db);
+        break;
+      case menuOptions[2]:
+        await updateEmployeeRole(db);
+        break;
+      case menuOptions[3]:
+        await viewAllRoles(db);
+        break;
+      case menuOptions[4]:
+        await addRole(db);
+        break;
+      case menuOptions[5]:
+        await viewAllDepartments(db);
+        break;
+      case menuOptions[6]:
+        await addDepartment(db);
+        break;
+      case menuOptions[7]:
+        process.exit();
+        break;
+    }
   }
 }
-
 init();
